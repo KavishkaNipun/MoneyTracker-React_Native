56d6e84f757d9988404fc97abdcd30cb4d8884a5
Kavishka Nipun <kavishkanipunofficial@gmail.com>
2021-07-09 18:23:40 -0700

    add signup screen and other features

--- scren/RootStackScreen.js
+++ scren/RootStackScreen.js
@@ -1,3 +1,23 @@
+// import React from 'react';
+
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// import SplashScreen from './SplashScreen';
+// import SignInScreen from './SignInScreen';
+// import SignUpScreen from './SignUpScreen';
+
+// const RootStack = createStackNavigator();
+
+// const RootStackScreen = ({navigation}) => (
+//     <RootStack.Navigator headerMode='none'>
+//        <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
+//        <RootStack.Screen name="SignInScreen" component={SignInScreen}/> 
+//         <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>  
+//     </RootStack.Navigator>
+// );
+
+// export default RootStackScreen;
+
 import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,7 +32,7 @@ const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator headerMode='none'>
         <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
         <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
-        {/* <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/> */} 
+        <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
     </RootStack.Navigator>
 );
 
